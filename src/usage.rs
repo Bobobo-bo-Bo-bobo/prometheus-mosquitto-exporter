dd4d3cb11570353f16a6639170f60b386c49cd71
Andreas Maus <maus@badphish.ypbind.de>
2022-03-23 16:30:55 +0100

    Updated crates and copyright year

--- src/usage.rs
+++ src/usage.rs
@@ -3,7 +3,7 @@ use crate::constants;
 pub fn show_version() {
     println!(
         "{} version {}
-Copyright (C) 2021 by Andreas Maus <maus@ypbind.de>
+Copyright (C) 2021-2022 by Andreas Maus <maus@ypbind.de>
 This program comes with ABSOLUTELY NO WARRANTY.
     
 {} is distributed under the Terms of the GNU General
